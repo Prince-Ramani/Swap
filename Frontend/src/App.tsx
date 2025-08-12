ba3d04c018c48f60f026f99c553f1a511b10e07e
Prince-Ramani <princeramani67@gmail.com>
2025-08-12 13:10:29 +0530

    I will complete it today.Maybeeeee

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -17,17 +17,50 @@ import Signin from "./pages/Signin";
 import Home from "./pages/Home";
 function App() {
   const { setAuthUser } = useAuthUser();
-  const { data, isLoading } = useQuery<userInterface | ApiError>({
+  const { data, isLoading, isError, error } = useQuery<
+    userInterface | ApiError
+  >({
     queryKey: ["authUser"],
     queryFn: async () => {
-      const res = await fetch("/api/auth/getMe");
-      const data: userInterface | ApiError = await res.json();
+      try {
+        const res = await fetch("/api/auth/getMe");
+        if (!res.ok) {
+          let errorMessage = "Something went wrong!";
+          try {
+            const errorData = await res.json();
+            if (errorData?.error && typeof errorData.error === "string")
+              errorMessage = errorData.error;
+          } catch (err) {
+            console.error("Failed to parse error response from server", err);
+            errorMessage =
+              "Failed to process authorization request. Please try again later.";
+          }
+          throw new Error(errorMessage);
+        }
+        const data: userInterface | ApiError = await res.json();
 
-      if (!!data && "_id" in data) setAuthUser(data);
-      return data;
+        if (!!data && "_id" in data) setAuthUser(data);
+        return data;
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          console.error("Error occured in authorization", err.message);
+          throw err;
+        } else {
+          console.error("Unexpected error occured in authorization", err);
+          throw new Error("Unexpected error occured in authorization");
+        }
+      }
     },
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
     retry: false,
   });
+
+  if (isError) {
+    console.error(
+      error.message || "Somethinf went wrong.Plesae try again later",
+    );
+  }
 
   const isAuthUser = !!data && "_id" in data;
 
@@ -48,6 +81,10 @@ function App() {
         />
 
         <Route path="/home" element={<Home />} />
+        <Route
+          path="*"
+          element={<Navigate to={isAuthUser ? "/home" : "/signup"} />}
+        />
       </Routes>
     </Router>
   );
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,6 +1,61 @@
 import { Button } from "@/components/ui/button";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const Home = () => {
+  type LogoutSuccessResponse = { message: string };
+  type LogoutErrorResponse = { error: string };
+  type LogoutResponse = LogoutSuccessResponse | LogoutErrorResponse;
+
+  const queryClient = useQueryClient();
+  const { data, isPending } = useMutation<LogoutResponse, Error>({
+    mutationFn: async () => {
+      try {
+        const res = await fetch("/api/auth/logout");
+        if (!res.ok) {
+          let errorMessage = "Something went wrong.";
+          try {
+            const errorData = await res.json();
+            if (errorData?.error && typeof errorData.error === "string") {
+              errorMessage = errorData.error;
+            }
+          } catch (error) {
+            console.error("Error parsing error response from server : ", error);
+            errorMessage =
+              "Failed to process logout response. Please try again later.";
+          }
+          throw new Error(errorMessage);
+        }
+        const data: LogoutResponse = await res.json();
+        return data;
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          console.error("Error during logout mutation : ", err.message);
+          throw err;
+        }
+        console.error("Unexpected error during logout : ", err);
+        throw new Error("An unexpected error occured while logging out.");
+      }
+    },
+
+    onSuccess: (data) => {
+      if ("error" in data) {
+        toast.error(data.error);
+        console.log("Logout failed", data.error);
+      } else {
+        toast.success(data.message);
+        queryClient.invalidateQueries({ queryKey: ["authUser"] });
+      }
+    },
+    onError: (error) => {
+      const errMessage =
+        error.message ||
+        "Unexpected error occured while logging out. Please try again later.";
+      toast.error(errMessage);
+      console.log("Logout error : ", errMessage);
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+    },
+  });
   return (
     <div>
       <div className="flex justify-end items-center p-5">
